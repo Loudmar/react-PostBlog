8d79bf43e37dce76dafcb181e79c88807f5c66fb
Loudmar <loud.tur@gmail.com>
2023-02-01 17:00:21 +0000

    Initial commit

---
+++ src/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function About() {
+  return (
+    <main className='About'>
+      <h2>About</h2>
+      <p style={{ marginTop: '1rem' }}>This post app is a project to work with React routes.</p>
+    </main>
+  )
+}
+
+export default About
---
+++ src/Missing.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Missing = () => {
+  return (
+    <main className='Missing'>
+      <h2>Page Not Found!</h2>
+      <p>We are sorry!</p>
+      <p><Link to='/'>Visit our Homepage.</Link></p>
+    </main>
+  )
+}
+
+export default Missing
